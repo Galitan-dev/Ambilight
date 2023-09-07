d851ff80afb8e9be245ff1bfb989e62fd5db3835
Galitan-dev <galitan.dev@gmail.com>
2023-09-07 20:13:32 +0200

    fix(server): faster

--- server/src/colorScheme.ts
+++ server/src/colorScheme.ts
@@ -5,7 +5,7 @@ export function colorScheme(): RequestHandler {
     return function(_, res) {
         const scheme: ColorScheme = {
             up: Array.from({ length: leds.horizontal }, (_, i) => 
-                [Math.round((i + Date.now() % 30000 / 30000 * leds.horizontal) % leds.horizontal / leds.horizontal * 255), 0, 0]),
+                [Math.round((i + Date.now() % 2000 / 2000 * leds.horizontal) % leds.horizontal / leds.horizontal * 255), 0, 0]),
             left: Array.from({ length: leds.vertical }, () => [0, 255, 0]),
             bottom: Array.from({ length: leds.horizontal }, () => [0, 0, 255]),
             right: Array.from({ length: leds.vertical }, () => [255, 0, 255]),

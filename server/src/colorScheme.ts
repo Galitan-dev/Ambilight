65886cc401c9726df522a9815d5022eadb975c84
Galitan-dev <galitan.dev@gmail.com>
2023-09-07 19:28:45 +0200

    fix(server): 255

--- server/src/colorScheme.ts
+++ server/src/colorScheme.ts
@@ -4,10 +4,10 @@ import { leds } from './config';
 export function colorScheme(): RequestHandler {
     return function(_, res) {
         const scheme: ColorScheme = {
-            up: Array.from({ length: leds.horizontal }, () => [100, 0, 0]),
-            left: Array.from({ length: leds.vertical }, () => [0, 100, 0]),
-            bottom: Array.from({ length: leds.horizontal }, () => [0, 0, 100]),
-            right: Array.from({ length: leds.vertical }, () => [100, 0, 100]),
+            up: Array.from({ length: leds.horizontal }, () => [255, 0, 0]),
+            left: Array.from({ length: leds.vertical }, () => [0, 255, 0]),
+            bottom: Array.from({ length: leds.horizontal }, () => [0, 0, 255]),
+            right: Array.from({ length: leds.vertical }, () => [255, 0, 255]),
         };
 
         res.send('[' + Object.values(scheme).flat(2).join(',') + ']')

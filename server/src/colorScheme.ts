4f78e172ce54cd5ba520c66017ab98ce560ff4a0
Galitan-dev <galitan.dev@gmail.com>
2023-09-07 20:08:24 +0200

    feat(server): dynamic

--- server/src/colorScheme.ts
+++ server/src/colorScheme.ts
@@ -4,7 +4,8 @@ import { leds } from './config';
 export function colorScheme(): RequestHandler {
     return function(_, res) {
         const scheme: ColorScheme = {
-            up: Array.from({ length: leds.horizontal }, () => [255, 0, 0]),
+            up: Array.from({ length: leds.horizontal }, (_, i) => 
+                [Math.round((i + Date.now() % 30000 / 30000 * leds.horizontal) % leds.horizontal / leds.horizontal * 255), 0, 0]),
             left: Array.from({ length: leds.vertical }, () => [0, 255, 0]),
             bottom: Array.from({ length: leds.horizontal }, () => [0, 0, 255]),
             right: Array.from({ length: leds.vertical }, () => [255, 0, 255]),
